dbac057cd39caa582ee3c653c8c219aabd9400e7
Erdane <mahobah.erdane@hotmail.fr>
2019-09-30 09:48:13 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+      </v-toolbar-title>    
       <v-spacer></v-spacer>
       <v-btn
         text
